a3a5a54a14a30ae6d5d5c524014be9a5904b1069
Naveen-AJ <111048972+naveen230230@users.noreply.github.com>
2025-03-14 21:59:36 +0530

    Update hello-world.java

--- hello-world.java
+++ hello-world.java
@@ -1,7 +1,16 @@
 package com.example;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World from WAR!</h1>");
     }
 }
